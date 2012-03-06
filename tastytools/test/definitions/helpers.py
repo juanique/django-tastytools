2343450bcb6316a98d55e7f33799570246a081c1
Juan Enrique Munoz <juanique@greedo.(none)>
2012-03-06 01:43:13 -0300

    fixed an issue that cuased nice errors test not to work properly when requiring authentication

--- tastytools/test/definitions/helpers.py
+++ tastytools/test/definitions/helpers.py
@@ -1,12 +1,9 @@
 def prepare_test_post_data(test, resource):
     post_data = resource.get_test_post_data()
 
-    if resource.method_requires_auth('POST'):
-        logged_in = '_auth_user_id' in test.client.session
-        if not logged_in:
-            try:
-                post_data = resource.setup_post(test, post_data)
-            except:
-                pass
- 
+    try:
+        post_data = resource._meta.testdata.setup_post(test, post_data) or post_data
+    except:
+        pass
+
     return post_data
