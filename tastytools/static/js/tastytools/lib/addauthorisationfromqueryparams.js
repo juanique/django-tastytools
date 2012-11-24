c11997b583758fbcca73c5272311f4b443a03399
Asim Hussain <jawache@gmail.com>
2012-11-24 23:13:20 +0000

    not so hacky any more :)

--- tastytools/static/js/tastytools/lib/addauthorisationfromqueryparams.js
+++ tastytools/static/js/tastytools/lib/addauthorisationfromqueryparams.js
@@ -4,8 +4,9 @@
  * So will work with APIAuthentication in tastypie
  */
 $.ajaxSetup({ beforeSend:function (xhr, settings) {
-    var user = "example";
-    var key = 1234;
+    var qp = $.deparam.querystring();
+    var user = qp.username;
+    var key = qp.api_key;
     if (user && key) {
         var token = "ApiKey " + user + ":" + key;
         console.log("Authorisation " + token);
